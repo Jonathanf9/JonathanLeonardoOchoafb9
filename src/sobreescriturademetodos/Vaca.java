5c184a9f0d60a78a423f589ce9420f49ed025f71
Lenovo <Lenovo@DESKTOP-2QHK8NG>
2020-09-24 16:52:04 -0500

    Sobreescritura de metodos , el proyecto consiste en sobreescribir los metodos y atributos de la clase principal con nombre Animal mediante , llamando a las subclases Vaca y Perro por lo tanto presentara una similitud de metodos y atributos mediante las subclases sin intervenir al presentar el proyecto para ello utilizado el metodo de implementacion como otros llamado ToString lo cual me va a retornar los objetos de la clase llamada Animal ademas tambien he utilizado el metodo el extends me va permitir heredar los atributos y metodos de la super clase llamada Animal , mediante las subclases Vaca y Perro.

---
+++ src/sobreescriturademetodos/Vaca.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sobreescriturademetodos;
+
+import sobreescriturademetodos.Animal;
+
+/**
+ *
+ * @author jonathanfb
+ */
+public class Vaca extends Animal {
+
+    public void comunicarse() {
+        System.out.println(" La vaca muge");
+
+    }
+
+    public void comer() {
+        System.out.println(" La vaca come pasto ");
+    }
+
+}
